532efa13fe8c0d49c3a3ee0a70a8fefb93c30e24
darthranjith <srivaranjith@gmail.com>
2019-08-14 18:00:04 +0530

    removed localhost cors

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const spicinemas = require('./routes/spicinemas');
 const app = express();
-var cors = require('cors');
 
-var corsOptions = {
-    origin: 'http://localhost:4200',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-var port = process.env.PORT || 3000;
-
-app.use(cors(corsOptions));
+const port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
     res.json({"OneScrapper": "REST API with node.js"});
